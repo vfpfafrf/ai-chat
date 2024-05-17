7fb6ecf9d5f8d13a00d8c03b749d7b352e7ae86c
Dmitry Ivanov <dmitry.ivanov@picsart.com>
2024-05-17 10:25:09 +0200

    initial commit

---
+++ src/main/kotlin/com/picsart/aichat/AiChatApplication.kt
@@ -0,0 +1,11 @@
+package com.picsart.aichat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiChatApplication>(*args)
+}
---
+++ src/test/kotlin/com/picsart/aichat/AiChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.picsart.aichat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
